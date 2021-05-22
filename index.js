1c34550f36ccc476ee8d078f2efefba2334d34bd
Ekow <ewusiessel@gmail.com>
2021-05-22 15:50:17 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const changeHeaderolours = document.querySelector('h1')
+
+
+
+function changeCol() {
+    changeHeaderolours.classList.add('red')
+    changeHeaderolours.classList.toggle('blue')
+    console.log('change')
+}
